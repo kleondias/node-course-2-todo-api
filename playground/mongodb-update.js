44f2572bcc6a16e7322d47a2d73a1c0ab2b0a2da
Kleon Dias <kdias76@hotmail.com>
2019-05-25 19:58:13 +0300

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,52 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client)=>{
+	if(err) {	
+		return console.log('Unable to connect to mongodb server');
+	}
+	console.log("Connected to mongodb Server");
+
+	const db = client.db('TodoApp');
+	
+	// db.collection('Todos').findOneAndUpdate({
+	// 	_id: new ObjectID('5ce970d70ad5d82e5ce8eb58')
+	// },{
+	// 	$set: {
+	// 		completed: true
+	// 	}
+	// },{	
+	// 	returnOriginal: false
+	// }).then((result)=>{
+	// 	console.log(result);
+	// });
+
+
+	db.collection('Users').findOneAndUpdate({
+		_id: new ObjectID('5ce91e182398e13c20f8dd0a')
+	},{
+		$set: {
+			name: 'Anastasia'
+		},
+		$inc:{
+			age:1
+		}
+	},{	
+		returnOriginal: false,
+		
+	}).then((result)=>{
+		console.log(result);
+	});
+
+	// client.close();
+});
+
+
+
+
+
+
+
+
+
