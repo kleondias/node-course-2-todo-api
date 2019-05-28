3cd9621c1d601a7b87bfd69ffd331d41558abcba
Kleon Dias <kdias76@hotmail.com>
2019-05-28 20:21:31 +0300

    Hash user password before saveing

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,39 @@
 // const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
 
-var data = {
-	id: 10
-};
+const bcrypt = require('bcryptjs');
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
 
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+var password = '123abc!';
+
+// bcrypt.genSalt(10, (err, salt)=>{
+// 	bcrypt.hash(password, salt, (err, hash)=>{
+// 		console.log(hash);
+// 	});
+// });
+
+
+var hashedPassword = '$2a$10$IU39GWfiiE9Em6Dc.ju3Re89PMt.Ub0zFRpoHRvWiI68iL1vIqwUe';
+
+bcrypt.compare('abd!', hashedPassword, (err, res)=>{
+	console.log(res);
+});
+
+
+
+
+
+
+
+// var data = {
+// 	id: 10
+// };
+
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
 
 /////////////////////////////////////////////////////
 // var message = 'I am user number 3'; 
